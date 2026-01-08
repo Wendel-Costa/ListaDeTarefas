24727d5238d15f550c98215d17a584ae01f8a609
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2026-01-08 01:34:23 +0000

    Criando TarefaController

---
+++ src/controller/TarefaController.js
@@ -0,0 +1,36 @@
+import { repositorioTarefas } from '../model/TarefasRepositorio.js';
+import { Tarefa } from '../model/Tarefa.js';
+import { StatusDisponivel } from '../model/status/StatusDisponivel.js';
+import { StatusFazendo } from '../model/status/StatusFazendo.js';
+import { StatusFeita } from '../model/status/StatusFeita.js';
+
+export class TarefaController {
+    constructor(view) {
+        this.view = view;
+        this.idAtual = 1;
+    }
+
+    criarTarefa(nome, descricao, statusTexto) {
+        const status = this.criarStatus(statusTexto);
+        const tarefa = new Tarefa(this.idAtual++, nome, descricao, status);
+        repositorioTarefas.adicionar(tarefa);
+        this.view.renderizar();
+    }
+
+    removerTarefa(id) {
+        repositorioTarefas.remover(id);
+        this.view.renderizar();
+    }
+
+    listarTarefas() {
+        return repositorioTarefas.listar();
+    }
+
+    criarStatus(texto) {
+        switch (texto) {
+            case 'Fazendo': return new StatusFazendo();
+            case 'Feita': return new StatusFeita();
+            default: return new StatusDisponivel();
+        }
+    }
+}
